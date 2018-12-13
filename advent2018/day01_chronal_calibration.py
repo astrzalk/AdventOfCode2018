5f0465f57b466a6cd28ccb76cf6fbb1999369856
Alexander Strzalkowski <alexander.strzalkowski@gmail.com>
2018-12-13 02:06:55 -0500

    Cleans up day01 code

--- advent2018/day01_chronal_calibration.py
+++ advent2018/day01_chronal_calibration.py
@@ -12,21 +12,12 @@ with open(file_name) as f:
         num = int(num.strip('\n'))
         inputs.append(num)
 
-def sum_freqs_part1(inputs):
-    return sum(inputs)
+assert sum([+1, -2, +3, +1]) == 3
+assert sum([+1, +1, +1]) == 3
+assert sum([+1, +1, -2]) == 0
+assert sum([-1, -2, -3]) == -6
 
-# Unit Tests
-test1 = [+1, -2, +3, +1]
-test2 = [+1, +1, +1]
-test3 = [+1, +1, -2]
-test4 = [-1, -2, -3]
-
-assert sum_freqs_part1(test1) == 3
-assert sum_freqs_part1(test2) == 3
-assert sum_freqs_part1(test3) == 0
-assert sum_freqs_part1(test4) == -6
-
-print("Solution to Part 1 is {}".format(sum_freqs_part1(inputs))) # 513
+print("Solution to Part 1 is {}".format(sum(inputs))) # 513
 
 def find_first_repeat_freq(inputs):
     cum_sum = 0
@@ -38,15 +29,10 @@ def find_first_repeat_freq(inputs):
         freqs_seen.add(cum_sum)
     return cum_sum
 
-test2_part2 = [1, -1]
-test3_part2 = [+3, +3, +4, -2, -4]
-test4_part2 = [-6, +3, +8, +5, -6]
-test5_part2 = [+7, +7, -2, -7, -4]
-
-assert find_first_repeat_freq(test1) == 2
-assert find_first_repeat_freq(test2_part2) == 0
-assert find_first_repeat_freq(test3_part2) == 10
-assert find_first_repeat_freq(test4_part2) == 5
-assert find_first_repeat_freq(test5_part2) == 14
+assert find_first_repeat_freq([+1, -2, +3, +1]) == 2
+assert find_first_repeat_freq([1, -1]) == 0
+assert find_first_repeat_freq([+3, +3, +4, -2, -4]) == 10
+assert find_first_repeat_freq([-6, +3, +8, +5, -6]) == 5
+assert find_first_repeat_freq([+7, +7, -2, -7, -4]) == 14
 
 print("Solution to Part 2 is {}".format(find_first_repeat_freq(inputs))) # 287
